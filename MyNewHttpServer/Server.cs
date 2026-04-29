77154d8b6a6e875e15e58c86db1b94ef5d0d2805
Natalya Babenko <ifelseiforelif@gmail.com>
2026-04-29 18:07:19 +0300

    added post requests

--- MyNewHttpServer/Server.cs
+++ MyNewHttpServer/Server.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using System.Text.Json;
 
 namespace MyNewHttpServer;
+
+class User
+{
+    public string Login { get; set; } = null!;
+    public string Pwd { get; set; } = null!;
+    public override string ToString()
+    {
+        return $"Login: {Login} Password {Pwd}";
+    }
+}
 
 internal class Server
 {
@@ -25,12 +36,23 @@ internal class Server
                 {
                     Console.WriteLine($"Request: {req.Url} {req.HttpMethod} {req.Url?.AbsolutePath}");
                     string? param = req.Url?.AbsolutePath;
-                    if(param==null)
+                    Console.WriteLine(param);
+                    if (param == null)
                     {
                         param = "/";
                     }
+                    if (param == "/")
+                    {
+                        // base url
+                        //query params
+                        var queryString = req.QueryString;
+                        if (queryString != null)
+                        {
+                            Console.WriteLine($"QUERY PARAMS : {queryString["login"]} {queryString["pwd"]}");
+                        }
+                    }
                     string page = GetPageName(param);
-                    
+
                     string path = Path.Combine(AppContext.BaseDirectory, "wwwroot", "pages", page);
                     string html = await File.ReadAllTextAsync(path, req.ContentEncoding);
                     byte[] bytes = Encoding.UTF8.GetBytes(html);
@@ -42,6 +64,32 @@ internal class Server
                         stream.Write(bytes, 0, bytes.Length);
                     }
                 }
+                else if (req.HttpMethod == "POST")
+                {//зчитування тіла запиту
+                    string body = "";
+                    using (var reader = new StreamReader(req.InputStream, req.ContentEncoding))
+                    {
+                        body = await reader.ReadToEndAsync();
+                        try
+                        {
+                            var user = JsonSerializer.Deserialize<User>(body);
+                            byte[] bytes = Encoding.UTF8.GetBytes(body);
+                            res.ContentLength64 = bytes.Length;
+                            res.ContentType = "application/json; charset=utf-8";
+                            res.StatusCode = 200;
+                            using (Stream stream = res.OutputStream)
+                            {
+                                stream.Write(bytes, 0, bytes.Length);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    
+                } 
                 res.Close();
             }
             catch(Exception ex)
